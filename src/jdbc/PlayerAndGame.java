d4b2293be25fa0e9a03eb80e24b55c38d9af0a1d
Aaron <lou22103@gmail.com>
2022-11-19 13:01:11 -0500

    player and game class

--- src/jdbc/PlayerAndGame.java
+++ src/jdbc/PlayerAndGame.java
@@ -2,6 +2,10 @@ package jdbc;
 
 public class PlayerAndGame {
 	private String playerGameId;
+	private String gameId;
+	private String playerId;
+	private String playingDate;
+	private String score;
 	
 
 }
